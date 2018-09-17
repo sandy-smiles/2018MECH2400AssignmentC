43c43cd8273c3de5da649975ba317c3b40fa3b56
kadi9940 <kadi9940@uni.sydney.edu.au>
2018-09-17 12:56:12 +1000

    Adding in the basic arduino robot base code with a few pre-defined defines.

---
+++ DEFINE/DEFINE.ino
@@ -0,0 +1,12 @@
+#include <math.h>
+
+
+// Defines for the Wheel values
+#define WHEEL_L_T
+#define WHEEL_L_B
+#define WHEEL_R_T
+#define WHEEL_R_B
+
+
+
+
---
+++ Robot_Main/Robot_Main.ino
@@ -0,0 +1,11 @@
+#include "DEFINE"
+
+#define SERIAL_SPEED 9600
+
+void setup() {
+  Serial.begin(SERIAL_SPEED);
+}
+
+void loop() {
+  
+}
