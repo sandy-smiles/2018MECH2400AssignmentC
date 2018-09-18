0d78694283d826be07eed14e74c84251a8d8b745
kadi9940 <kadi9940@uni.sydney.edu.au>
2018-09-18 12:23:04 +1000

    Deleting the test NEW_FILE.txt due to it doing nothing and updated Robot_Main to include initialising pinModes.

--- Robot_Main/Robot_Main.ino
+++ Robot_Main/Robot_Main.ino
@@ -3,9 +3,19 @@
 #define SERIAL_SPEED 9600
 
 void setup() {
+  // Setup communications back to computer for testing.
   Serial.begin(SERIAL_SPEED);
+  
+  // Initialise the servos and the other ports as input or output
+  pinMode(LED, 13); // Inbuilt led onto the arduino board (from memory)
+  pinMode(WHEEL_L_T, OUTPUT);
+  pinMode(WHEEL_L_B, OUTPUT);
+  pinMode(WHEEL_R_T, OUTPUT);
+  pinMode(WHEEL_R_B, OUTPUT);
 }
 
 void loop() {
-  
+  while (TRUE) {
+    drive(wheelSpeed); // Initially just try to get the robot driving in a straight line.
+  }
 }
